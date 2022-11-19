1a79159fefcfa4e8e32c7fa02599a28d0f84007d
Forever512h <118043926+Forever512h@users.noreply.github.com>
2022-11-19 05:34:41 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+let a = 30;
+while (a) { // выводит 0, затем 2, затем 4
+ console.log('resulat ' + a );
+ a -= 2; //i = i - 2;
+}
+
+let i = 0;
+while (i < 10) { // выводит 0, затем 2, затем 4
+ console.log('resulat ' + i );
+ i += 2; //i = i + 2;
+}
+
+
